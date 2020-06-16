4d6fa345221f75975bc262979eded411bfd46701
magic3030 <lidin1911@gmail.com>
2020-06-16 14:55:00 +0800

    1454

--- magic_1090616-1.js
+++ magic_1090616-1.js
@@ -1,4 +1,11 @@
 //magic 1090616 1410
-//1413
-//1417
-//1426
+data_1 =100
+function f1(){
+    var a1=200
+    a2=300
+    console.log(a1)
+    console.log(a2)
+}
+console.log(data_1)
+console.log(a1)
+console.log(a2)
